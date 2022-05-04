27b088ff8f6e5d1fa52c131a19640cd7cf2b4682
Collin Rukundo <support@matchstick.ug>
2022-05-04 23:55:06 +0300

    Added form that stores data in object

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,5 +1,12 @@
+import NewMeetupForm from "../components/meetups/NewMeetupForm";
+
 function NewMeetupsPage() {
-    return <div>NewMeetupsPage</div>;
+    return (
+        <section>
+            <h1>Create New Meetup</h1>
+            <NewMeetupForm />
+        </section>
+    );
 }
 
 export default NewMeetupsPage;
