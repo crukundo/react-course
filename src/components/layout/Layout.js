2ae5e524363e6e3bf97db1518a44ac40213c5d09
Collin Rukundo <support@matchstick.ug>
2022-05-04 23:27:23 +0300

    Added wrapped components

---
+++ src/components/layout/Layout.js
@@ -0,0 +1,11 @@
+import classes from './Layout.module.css'
+import MainNav from './MainNav'
+
+function Layout(props) {
+    return <div>
+        <MainNav />
+        <main className={classes.main}>{props.children}</main>
+    </div>
+}
+
+export default Layout;
--- src/components/layout/MainNav.js
+++ src/components/layout/MainNav.js
@@ -1,21 +1,26 @@
 import { Link } from 'react-router-dom';
+
+import classes from './MainNav.module.css';
+
 function MainNav() {
-    return <header>
-        <div>React Meetups</div>
-        <nav>
-            <ul>
-                <li>
-                    <Link to='/'>All Meetups</Link>
-                </li>
-                <li>
-                    <Link to='/new'>Add New Meetup</Link>
-                </li>
-                <li>
-                    <Link to='/favorites'>My Favorites</Link>
-                </li>
-            </ul>
-        </nav>
-    </header>
+    return (
+        <header className={classes.header}>
+            <div className={classes.logo}>React Meetups</div>
+            <nav>
+                <ul>
+                    <li>
+                        <Link to='/'>All Meetups</Link>
+                    </li>
+                    <li>
+                        <Link to='/new'>Add New Meetup</Link>
+                    </li>
+                    <li>
+                        <Link to='/favorites'>My Favorites</Link>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    );
 }
 
 
